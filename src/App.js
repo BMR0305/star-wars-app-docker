cfe072021be15546ea41b48001b2fc0d1e74c070
BMR0305 <67845677+BMR0305@users.noreply.github.com>
2020-12-16 19:15:29 -0600

    Actividad 4

--- src/App.js
+++ src/App.js
@@ -33,8 +33,7 @@ export default class App extends React.Component {
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
-          >   
-          Hola!!!         
+          >            
           </a>
           <div>{this.state.counter}</div>
           <Alert count={this.state.counter}></Alert>
